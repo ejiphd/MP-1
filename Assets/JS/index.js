63b569aa88bd0b2c79d1b161eac2a40896db0d98
Dizon, Ejiph B <64470910+ejiphd@users.noreply.github.com>
2023-03-15 13:00:42 +0800

    Add files via upload
    final home page

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -7,3 +7,48 @@ window.addEventListener('scroll', function(){
         nav.classList.remove('nav-scrolled','shadow');
     }
 });
+
+// home page book now
+var subjectObject = {
+    "Luzon":{
+        "Baguio" : "Baguio",
+        "Batanes" : "Batanes",
+        "Batangas" :"Batangas",
+        "Ilocos Norte" : "Ilocos Norte",
+        "Pangasinan" : "Pangasinan" ,
+        "Vigan" : "Vigan",
+    },
+    "Visayas":{
+        "Biliran" : "Biliran",
+        "Bohol" : "Bohol",
+        "Cebu" : "Cebu",
+        "Negros Occidental" : "Negros Occidental",
+        "Samar" : "Samar",
+    },
+    "Mindanao":{
+        "Cagayan De Oro" : "Cagayan De Oro",
+        "Davao" : "Davao",
+        "Iligan City" : "Iligan City",
+        "Siargao" : "Siargao",
+        "Surigao Del Sur" : "Surigao Del Sur",
+        "Tagum" : "Tagum"
+    }
+}
+
+window.onload = function(){
+    var choose = document.getElementById('destination')
+    var places = document.getElementById('places')
+
+    for(var x in subjectObject){
+        destination.options[destination.options.length] = new Option(x)
+    }
+
+    choose.onchange = function(){
+        places.length = 1
+
+        for (var y in subjectObject[this.value]){
+            places.options[places.options.length] = new Option(y)
+
+        }
+    }
+}
